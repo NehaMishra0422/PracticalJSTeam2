67023629e58016367f7672e30bfa61bc4b4a4868
NehaMishra0422 <nehamishra0422@gmail.com>
2026-08-25 22:20:21 +0530

    second commit of JSPracticalTeam2

---
+++ Objects/accessingObjectProperties.js
@@ -0,0 +1,12 @@
+//using .notation
+let obj = {
+    fullName : "Neha Mishra",
+    age : 31
+};
+console.log(obj); //{fullName : "Neha Mishra", age : 31}
+console.log(obj.fullName); //Neha Mishra
+console.log(obj.age); //31
+
+//using [] notation
+console.log(obj["fullName"]); //Neha Mishra
+console.log(obj["age"]);  //31
