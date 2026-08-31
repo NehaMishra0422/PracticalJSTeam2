2b6c5a5bf8a8f06cbeaad2d85f1bebad259f0fa9
NehaMishra0422 <nehamishra0422@gmail.com>
2026-08-31 12:46:38 +0530

    sixth commit of JS Practice Team 2

--- Objects/objectMethod.js
+++ Objects/objectMethod.js
@@ -51,3 +51,80 @@ let className = { cname :5 };
 Object.assign(student, rollNo , className );
 console.log(student);
 console.log(student.no);
+
+//assign()
+let target = { a:3 , b : 5};
+let source = { b : 10, c : 15};
+let returnedTarget = Object.assign(target,source);
+console.log(target); //{ a: 3, b: 10, c: 15 }
+
+//when targer parameter is null or undefiend 
+let t1 = { a : null, b : undefined};
+let s1 = { b : 10, c : 20};
+let rT = Object.assign(t1,s1);
+console.log(t1); //{ a: null, b: 10, c: 20 }
+
+//.getOwnPropertyDescriptor() method
+// this method tells the properties/attributes of a specific property inside an object.
+let car = {
+    name : "Morris Garages",
+    model : "MG Hector",
+    color : "white"
+}
+console.log(Object.getOwnPropertyDescriptor(car,"model"));
+// output : {
+//     value: 'MG Hector', --> current value of the property
+//   writable: true,   --> if true, the value of the proprty can be changed
+//   enumerable: true,  --> if true, the property is included in a for...in loop
+//   configurable: true --> if true, the property can be deleted or changed to an accessor property
+// }
+
+//example for writable
+car.model = "MG ZS EV";
+console.log(car.model); //MG ZS EV
+console.log(Object.getOwnPropertyDescriptor(car, "model"));
+//output :
+// {
+//   value: 'MG ZS EV',
+//   writable: true,
+//   enumerable: true,
+//   configurable: true
+// }
+
+//.defineProperty() method --> defines a new property or modify an existing property on an object and return the object
+const obj = {};
+console.log(Object.defineProperty(obj, "code" ,{
+    value : 55,
+    writable : false,
+}));
+console.log(obj);
+console.log(obj.code); //55
+obj.code = 90;
+console.log(obj.code); //55, because we had set property writable to false.
+
+//.hasOwn() --> checks whether an object has property directly on itself or not
+let child = {
+    name : "Shritik SHukla",
+    age : " 1 month",
+    city : "Noida",
+}
+console.log(Object.hasOwn(child, "weight")); //false
+console.log(Object.hasOwn(child , "age")); //true
+
+//Object.freeze() --> using this method, you cannot aadd, delete or modify property
+// Object.freeze(child);
+// child.age = 10;
+// console.log(child.age); //1 month
+// console.log(delete child.city); //false
+// console.log(child); //{ name: 'Shritik SHukla', age: ' 1 month', city: 'Noida' }
+// child.weight = "4kg";
+// console.log(child); //{ name: 'Shritik SHukla', age: ' 1 month', city: 'Noida' }
+
+//Object.seal() --> we can't add or delete property but can modify
+Object.seal(child);
+child.age = 10;
+console.log(child);
+// delete child.city;
+// console.log(child);
+// child.weight = "4kgs";
+// console.log(child);
