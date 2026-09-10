2126c10f08c755f4a3fa4179d714f1f81ce809cb
NehaMishra0422 <nehamishra0422@gmail.com>
2026-09-10 12:22:17 +0530

    30th commit of JS Practice Team 2

--- AsynchronousJavascript/generator.js
+++ AsynchronousJavascript/generator.js
@@ -1,6 +1,10 @@
 //Generator - is a special function created using function* that can pause and resume execution using yield.
 //way to create iterator
 // Generators automatically return an iterator.
+//Generator can return multiple values one by one
+//when generator function is called,it returns a generator object not a value like { value: 10, done: false }
+//with result.next() -> manually get next value
+//with for..of loop -> automatically get yielded value,ie it automatically calls .next() until done : true
 
 //example 1
 function* numbers(){      //function* this tells JS that this is a generator function
@@ -8,8 +12,32 @@ function* numbers(){      //function* this tells JS that this is a generator fun
     yield 20;
     yield 30;
 } 
-let result = numbers();
-console.log(result.next());
-console.log(result.next());
-console.log(result.next());
-console.log(result.next());
+ let result = numbers();
+ console.log(result.next());
+ console.log(result.next());
+ console.log(result.next());
+ console.log(result.next());
+/* output
+{ value: 10, done: false }
+{ value: 20, done: false }
+{ value: 30, done: false }
+{ value: undefined, done: true }
+*/ 
+
+//with  for...of loop
+function* num(){      //function* this tells JS that this is a generator function
+    yield 10;  //yield means give the value and PAUSE, and resumes from where it has paused
+    yield 20;
+    yield 30;
+} 
+ let result1 = num();
+
+for(let value of num()){
+    //console.log(result1.next());  // doesn't required in for...of loop
+// { value: 10, done: false }
+// { value: 20, done: false }
+// { value: 30, done: false }
+    console.log(value); //10
+                        //20
+                        //30
+}
--- AsynchronousJavascript/iterator.js
+++ AsynchronousJavascript/iterator.js
@@ -2,6 +2,7 @@
 //It has special method next() -> returns object with two properties
 // 1. value : actual current value
 // 2. done : boolean value(true/false), if true it means sequence finished otherwise it will give false
+//Symbol.iterator - used to get the iterator from an iterable object
 
 //example 1
 const num = [10,30,50,70,25,55,60];
@@ -24,4 +25,26 @@ console.log(iterate.next());
 { value: 60, done: false }
 { value: undefined, done: true }
  */
+
+//Iterator.from() -> create iterator object from existing iterable
+//converts given value into iterator
+let myIterator = Iterator.from([5,10,15,20,25]); //
+let element = "";
+for(let x of myIterator){
+   element+= x + " ";
+}
+console.log(element); //5 10 15 20 25
+
+//every(fn) -> method returns true if all elements in the iterator satisfy the provided test function.
+const myIterator1 = Iterator.from("123456789"); // Create an Iterator
+let result = myIterator1.every(x => x > 7); //// Is every Element greater than 7?
+console.log(result); //false
+
+//find(fn) -> method returns the first element that satisfies a test function.
+const myIterator2 = Iterator.from([3, 10, 18, 30, 20]);
+let result1 = myIterator2.find(x => x > 18);// Find first greater than 18
+console.log(result1); //30
+
+
+
 
