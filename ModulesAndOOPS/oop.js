d049619ad12b9aba949364eea68b97acc2aec083
NehaMishra0422 <nehamishra0422@gmail.com>
2026-09-16 12:12:58 +0530

    37th commit of JS Practice Team 2

--- ModulesAndOOPS/oop.js
+++ ModulesAndOOPS/oop.js
@@ -1,9 +1,12 @@
 //OOP - Object Oriented Progamming.
 // A way of writing programs by organizing code around objects
 //An object can contain : Data -> properties, Actions -> methods
-//constructor -> a method that automatically runs when an object is created
+//constructor -> a method that automatically runs when an object is created using 'new'
+// we use constructor to initialize the property of object
 //new -> keyword to create an instance of a class ie new object which triggers the constructor.
 //this -> to reference the current object
+//in simple way :
+// 'new' creates the object → 'constructor' initializes it → 'this' refers to that object.
 
 //example
 class user{
