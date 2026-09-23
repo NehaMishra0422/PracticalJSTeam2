0b984c0c8c85ad298f3dc68b05e80ae85e962fd2
NehaMishra0422 <nehamishra0422@gmail.com>
2026-09-23 14:21:40 +0530

    48th commit of JS Practice Team 2

---
+++ CSS_Selectors/class.js
@@ -0,0 +1,34 @@
+/*
+WESITE USED : https://datatables.net/examples/datetime/
+
+class denoted by dot(.)
+
+Multiple class
+<button class="btn primary">Login</button>
+
+.btn.primary
+
+
+1. Database Tables 
+.dt-demo-selector__current 
+
+2. Amazon 
+Your Browsing history
+multiple class
+.a-size-base.a-inline-block
+
+3. space between 2 class
+
+when an element with class is inside element with class (parent -> child)
+ex - .a-button-stack .a-button:first-child
+
+4. element + class
+
+button.a-button-text
+
+5. class + ID
+ID -> a-autoid-1-announce
+
+#a-autoid-1-announce.a-button-text
+
+*/
--- CSS_Selectors/userInput.js
+++ CSS_Selectors/userInput.js
@@ -9,3 +9,51 @@
 
 //input='name'
 
+//using function
+/*
+User gives a value → function uses that value → function creates a CSS selector → Playwright uses that selector.
+
+
+1. function createSelectot(tagName){
+return '#' + tagName;
+}
+
+let selector = createSelector(tagName);
+console.log(selector);
+
+2. or using prompt
+
+let input = prompt("Enter Username");
+function createSelectot(tagName){
+return '#' + tagName;
+}
+
+let selector = createSelector(input);
+console.log(selector);
+
+3. to use this in playwright :
+let input = prompt("Enter Username");
+function createSelectot(tagName){
+return '#' + tagName;
+}
+
+let selector = createSelector(input);
+console.log(selector);
+
+await page.locator(selector).click();
+
+
+4. or with Template literals : -
+
+let input = prompt("Enter Username");
+function createSelectot(tagName){
+return `#${tagName}`;
+}
+
+let selector = createSelector(input);
+console.log(selector);
+
+await page.locator(selector).click();
+
+
+*/
